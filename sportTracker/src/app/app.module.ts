083a7e29f09da33b35d240910657dfc0b236a7e3
nored91 <nored91>
2017-10-06 10:05:24 +0200

    Commit routing

--- sportTracker/src/app/app.module.ts
+++ sportTracker/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { RouterModule }   from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    DetailsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+    {
+      path: 'en-savoir-plus',
+      component: DetailsComponent
+    },
+    {
+      path: '',
+      component: HomeComponent
+    }
+])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
